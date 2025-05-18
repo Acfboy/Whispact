da96479477f0ca32b74f84a1fc017bffa16d1d8b
hxh7 <1036091334@qq.com>
2025-05-18 21:41:31 +0800

    可恶，居然还没有通信成功。 我又得交一发

--- src/views/PromptPage.vue
+++ src/views/PromptPage.vue
@@ -6,9 +6,32 @@
  </v-container>
    <v-container fluid>
      <v-textarea label="属于你们的打卡瞬间" counter auto-grow class="ma-6"></v-textarea>
-        <v-row justify="center" class="ma-5">
-           <v-btn @click="add-textarea" rounded block>确定</v-btn>
-        </v-row>
+      <v-dialog max-width="500" persistent transition="dialog-bottom-transition">
+        <template v-slot:activator="{ props: activatorProps }">
+          <v-row justify="center" class="ma-5">
+            <v-btn 
+             v-bind="activatorProps"
+             rounded
+             block>确定
+            </v-btn>
+          </v-row>
+       </template>
+       <template v-slot:default="{ isActive }">
+           <v-card>
+             <v-card-text>
+               请与另一台手机碰一碰
+             </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="退出"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+       </template>
+      </v-dialog>
  </v-container>
 </template>
 
